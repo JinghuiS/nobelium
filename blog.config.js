74440075f391b56094af80e722f6c004c218a6f1
石京辉 <1292077619@qq.com>
2021-12-19 21:19:43 +0800

    修复js风格

--- blog.config.js
+++ blog.config.js
@@ -1,15 +1,15 @@
 const BLOG = {
-  title: "link-blog",
-  author: "link",
-  email: "1292077619@qq.com",
-  link: "https://link-blog.vercel.app",
-  description: "真不戳",
-  lang: "zn-CN", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  appearance: "auto", // ['light', 'dark', 'auto'],
-  font: "sans-serif", // ['sans-serif', 'serif']
-  lightBackground: "#ffffff", // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: "#18181B", // use hex value, don't forget '#'
-  path: "", // leave this empty unless you want to deploy Nobelium in a folder
+  title: 'link-blog',
+  author: 'link',
+  email: '1292077619@qq.com',
+  link: 'https://link-blog.vercel.app',
+  description: '真不戳',
+  lang: 'zn-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  appearance: 'auto', // ['light', 'dark', 'auto'],
+  font: 'sans-serif', // ['sans-serif', 'serif']
+  lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
+  darkBackground: '#18181B', // use hex value, don't forget '#'
+  path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
   postsPerPage: 7,
   sortByDate: false,
@@ -17,46 +17,46 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL:
-    "https://og-image-craigary.vercel.app/**Link**%20Blog.png?theme=light&md=1&fontSize=175px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-light-bg.svg&images=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2Fremojansen%2Flogo.ts%40master%2Fts.svg", // The link to generate OG image, don't end with a slash
-  socialLink: "https://twitter.com/Jinghui_Link",
+    'https://og-image-craigary.vercel.app/**Link**%20Blog.png?theme=light&md=1&fontSize=175px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-light-bg.svg&images=https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2Fremojansen%2Flogo.ts%40master%2Fts.svg', // The link to generate OG image, don't end with a slash
+  socialLink: 'https://twitter.com/Jinghui_Link',
   seo: {
-    keywords: ["Blog", "Website", "Notion"],
-    googleSiteVerification: "", // Remove the value or replace it with your own google site verification code
+    keywords: ['Blog', 'Website', 'Notion'],
+    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: "", // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
-      tracker: "", // e.g 'https://ackee.craigary.net/tracker.js'
-      dataAckeeServer: "", // e.g https://ackee.craigary.net , don't end with a slash
-      domainId: "", // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+      tracker: '', // e.g 'https://ackee.craigary.net/tracker.js'
+      dataAckeeServer: '', // e.g https://ackee.craigary.net , don't end with a slash
+      domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: "", // e.g: G-XXXXXXXXXX
-    },
+      measurementId: '' // e.g: G-XXXXXXXXXX
+    }
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: "", // leave it empty if you don't need any comment plugin
+    provider: '', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
-      repo: "", // The repository of store comments
-      owner: "",
+      repo: '', // The repository of store comments
+      owner: '',
       admin: [],
-      clientID: "",
-      clientSecret: "",
-      distractionFreeMode: false,
+      clientID: '',
+      clientSecret: '',
+      distractionFreeMode: false
     },
     utterancesConfig: {
-      repo: "",
+      repo: ''
     },
     cusdisConfig: {
-      appId: "", // data-app-id
-      host: "https://cusdis.com", // data-host, change this if you're using self-hosted version
-      scriptSrc: "https://cusdis.com/js/cusdis.es.js", // change this if you're using self-hosted version
-    },
+      appId: '', // data-app-id
+      host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
+      scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
+    }
   },
-  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-};
+  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+}
 // export default BLOG
-module.exports = BLOG;
+module.exports = BLOG
